0097a2203dbeabcd690c8b7f961bb82db3780564
marksutt <marksutt@7c7fe9aa-52eb-4d9e-b0a8-ba7d787348e9>
2010-06-30 15:47:40 +0000

    Calculate D based on position of reference group and second group of particles (e.g. viruses)

--- plugin/src/mosaic/interaction/InteractionPlugin.scala
+++ plugin/src/mosaic/interaction/InteractionPlugin.scala
@@ -11,7 +11,7 @@ import scalala.Scalala._
 import scalala.tensor.dense._
 
 class InteractionPlugin extends PlugIn with ImagePreparation {
-	val d = 3 //Image dimensions
+	val dim = 3 //Image dimensions
 	
 	@Override
 	def run(arg: String) {
@@ -19,30 +19,22 @@ class InteractionPlugin extends PlugIn with ImagePreparation {
 		allocateTwoImages();
 		detect();
 		val (d, qOfD) = calculateQofD()
+		val dd = findD()
 		
-//		D with NN
 //		Select potential
 //		nll optimization CMA
 	}
 	
 //		qOfD with NN and Kernelest
 	def calculateQofD():(Array[Double],Array[Double])= {
-	  val nn = new NearestNeighbour(d)
-	  var refPoints : Array[Array[Double]] = getParticlePositions(0)
 	  
 	  val nbrQueryPoints = 100
 	  // independent randomly placed query objects
-	  val queryPoints = Array.fill(nbrQueryPoints)(rand(d).toArray)
+	  val queryPoints = Array.fill(nbrQueryPoints)(rand(dim).toArray)
 	  // regularly placed query objects
 //	  val queryPoints = nn.getMesh(List((1, 1*nbrQueryPoints),(1, 2*nbrQueryPoints),(1, 2*nbrQueryPoints)))
 	  
-	  val time = (new java.util.Date()).getTime()
-	  // generate KDTree
-	  nn.setReferenceGroup(refPoints)
-	  println("Generation KDtree "+((new java.util.Date()).getTime() - time)*0.001)
-	  // find NN
-      val dist = nn.getDistances(queryPoints) 
-      println("Generation + Search KDtree "+((new java.util.Date()).getTime() - time)*0.001)
+	  val dist = getDistances(queryPoints)
       
       // estimate q(d)
 	  val est = new KernelDensityEstimator()
@@ -56,6 +48,25 @@ class InteractionPlugin extends PlugIn with ImagePreparation {
 	  val prob = est.getProbabilities(xArray)
 	  plot(x, new DenseVector(prob))
 	  (xArray, prob)
+	}
+	
+	//		D with NN
+	def findD():Array[Double]= {
+	  val queryPoints: Array[Array[Double]] = getParticlePositions(1)
+	  getDistances(queryPoints)
+	}
+	
+	def getDistances(queryPoints: Array[Array[Double]]):Array[Double]= {
+			val nn = new NearestNeighbour(dim)
+			val refPoints : Array[Array[Double]] = getParticlePositions(0)
+			val time = (new java.util.Date()).getTime()
+			// generate KDTree
+			nn.setReferenceGroup(refPoints)
+			println("Generation KDtree "+((new java.util.Date()).getTime() - time)*0.001)
+			// find NN
+			val dist = nn.getDistances(queryPoints) 
+			println("Generation + Search KDtree "+((new java.util.Date()).getTime() - time)*0.001)
+			dist
 	}
 	
 	/**
