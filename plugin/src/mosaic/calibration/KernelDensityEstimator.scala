ad306fe320ca52a7b5157e7193715f1dc09744fb
marksutt <marksutt@7c7fe9aa-52eb-4d9e-b0a8-ba7d787348e9>
2010-09-20 06:29:43 +0000

    Wider kernels for estimator.

--- plugin/src/mosaic/calibration/KernelDensityEstimator.scala
+++ plugin/src/mosaic/calibration/KernelDensityEstimator.scala
@@ -6,16 +6,17 @@ import weka.estimators.KernelEstimator
 	 * <br>KernelDensityEstimater class is a wrapper for the KernelEstimator from the WEKA library
 	 */
 class KernelDensityEstimator {
+	val precision = 100d //TODO fix precision, gives wrong results if not equal to 1
 	
-	val est = new KernelEstimator(1) //TODO fix precision, gives wrong results if not equal to 1
+	val est = new KernelEstimator(1/precision) 
 	
 	def addValues(values : Array[Double]) {
 		values.foreach(est.addValue(_, 1))
 	}
 	
-	def getProbability(data: Double) = est.getProbability(data)// * 100 //TODO find explication why probablities have wrong scale?
+	def getProbability(data: Double) = est.getProbability(data) * precision // * 100 //TODO find explication why probablities have wrong scale?
 	
 	def getProbabilities(data : Array[Double]): Array[Double] = {
-		for (i <- data) yield est.getProbability(i)// * 100 //TODO find explication why probablities have wrong scale?
+		for (i <- data) yield est.getProbability(i) * precision// * 100 //TODO find explication why probablities have wrong scale?
 	}
 }
