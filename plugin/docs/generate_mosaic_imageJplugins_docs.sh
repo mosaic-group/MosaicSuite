8e2479ce4ff8f40d744f873d9af934b72f3bd018
janickc <janickc@7c7fe9aa-52eb-4d9e-b0a8-ba7d787348e9>
2010-11-27 16:34:37 +0000

    - revised text. - added script for generating a zip file for web page.

---
+++ plugin/docs/generate_mosaic_imageJplugins_docs.sh
@@ -0,0 +1,11 @@
+zip mosaic_ImageJplugins_docs.zip /home/janickc/phd/svn/ip/ImageJ/plugin/docs/Bessel_PSF/manual/BesselPSFConvolver_manual.pdf
+zip mosaic_ImageJplugins_docs.zip /home/janickc/phd/svn/ip/ImageJ/plugin/docs/BGS/manual/BGS_manual.pdf
+zip mosaic_ImageJplugins_docs.zip /home/janickc/phd/svn/ip/ImageJ/plugin/docs/ParticleTracker_3D/manual\ addendum/ParticleTracker_manual_addendum.pdf
+zip mosaic_ImageJplugins_docs.zip /home/janickc/phd/svn/ip/ImageJ/plugin/docs/ParticleTracker_3D/sample\ data/kinetochores/Cell1_5frames
+zip mosaic_ImageJplugins_docs.zip /home/janickc/phd/svn/ip/ImageJ/plugin/docs/ParticleTracker_3D/sample\ data/kinetochores/description.txt
+zip mosaic_ImageJplugins_docs.zip /home/janickc/phd/svn/ip/ImageJ/plugin/docs/PoissonNoise/manual/PoissonNoise_manual.pdf
+zip mosaic_ImageJplugins_docs.zip /home/janickc/phd/svn/ip/ImageJ/plugin/docs/PSF_measurement_3D/PSF_measurement_3D.pdf
+zip mosaic_ImageJplugins_docs.zip /home/janickc/phd/svn/ip/ImageJ/plugin/docs/PSF_Tool/sample_image/sample.tif
+zip mosaic_ImageJplugins_docs.zip /home/janickc/phd/svn/ip/ImageJ/plugin/docs/PSF_Tool/Tutorial/PSFtool.pdf
+
+
