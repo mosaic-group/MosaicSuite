ddba3ced5f4411963781b2557857c71d68dcd3f4
semmlers <semmlers@7c7fe9aa-52eb-4d9e-b0a8-ba7d787348e9>
2011-05-13 11:36:06 +0000

    commit test

--- plugin/src/mosaic/region_competition/ContourParticle.java
+++ plugin/src/mosaic/region_competition/ContourParticle.java
@@ -1,10 +1,8 @@
-package mosaic.region_competition;
 
-public class ContourParticle {
-	int mIntensity = 0;
-	int mLabel = 0;
-	int mCandidateLabel = 0;
-	float vEnergyDifference = 0f;
-	
-	
+public class ContourParticle 
+{
+	int intensity = 0;
+	int label = 0;
+	int candidateLabel = 0;
+	float energyDifference = 0f;
 }
