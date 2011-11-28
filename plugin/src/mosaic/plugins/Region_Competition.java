69b3d76780769144725d81f5f3487456c4458dcb
semmlers <semmlers@7c7fe9aa-52eb-4d9e-b0a8-ba7d787348e9>
2011-11-28 14:35:49 +0000

    added GUI (debugging purposes) save and reload initial guess

--- plugin/src/mosaic/plugins/Region_Competition.java
+++ plugin/src/mosaic/plugins/Region_Competition.java
@@ -4,20 +4,15 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.util.List;
 
-import mosaic.region_competition.Connectivity;
-import mosaic.region_competition.ConnectivityOLD;
-import mosaic.region_competition.ConnectivityOLD_2D_4;
-import mosaic.region_competition.LabelImage;
-import mosaic.region_competition.Pair;
-import mosaic.region_competition.Point;
-import mosaic.region_competition.Timer;
-import mosaic.region_competition.TopologicalNumberImageFunction;
-import mosaic.region_competition.UnitCubeCCCounter;
+import mosaic.region_competition.*;
 import ij.IJ;
 import ij.ImagePlus;
 import ij.ImageStack;
 import ij.WindowManager;
+import ij.gui.ImageCanvas;
 import ij.gui.Roi;
+import ij.io.FileSaver;
+import ij.io.Opener;
 import ij.plugin.filter.PlugInFilter;
 import ij.process.ImageProcessor;
 import ij.process.ShortProcessor;
@@ -28,19 +23,27 @@ import ij.process.ShortProcessor;
  * @version 14.5.2011
  */
 
-public class Region_Competition implements PlugInFilter{
+public class Region_Competition implements PlugInFilter
+{
 		
+	Region_Competition MVC;		// interface to image application (imageJ)
+	LabelImage labelImage;		// data structure mapping pixels to labels
+	ImagePlus originalIP;		// IP of the input image
+	ImageStack stack;			// stack saving the segmentation progress images
+	ImagePlus stackImPlus;		// IP showing the stack
 	
-	Region_Competition MVC;
-	LabelImage labelImage;
-	ImagePlus originalIP;
-	ImageStack stack;
-	ImagePlus stackImPlus;
+	UserDialog userDialog;
 	
 	
 	public int setup(String aArgs, ImagePlus aImp)
 	{
 		
+//		IJ.showMessage("version 2011 11 15");
+		
+		userDialog = new UserDialog("starterpanel");
+		userDialog.showDialog();
+		
+				
 		MVC=this;
 		originalIP = aImp;
 
@@ -56,20 +59,15 @@ public class Region_Competition implements PlugInFilter{
 			while(originalIP == null) 
 			{
 				//file not found, open menu
-//			IJ.showMessage("File "+fileName+" not found of incompatible, please select manually");
+//			IJ.showMessage("File "+fileName+" not found or incompatible, please select manually");
 				IJ.open();
 				originalIP = WindowManager.getCurrentImage();
 			} 
 			
 		}
 		
-//		manualSelect();
-
-		initStackIP();
 		frontsCompetitionImageFilter();
 
-		labelImage.showStatistics();
-//		macroContrast();
 		return DOES_ALL + DONE;
 	}
 	
@@ -90,31 +88,77 @@ public class Region_Competition implements PlugInFilter{
 		labelImage = new LabelImage(MVC);
 		
 		labelImage.initZero();
+		
+		if(userDialog.doUser)
+		{
+			System.out.println("manualSelect");
+			manualSelect(labelImage);
+		}
+		else if(userDialog.doRand)
+		{
+			labelImage.initialGuessRandom();
+		}
+		else if(userDialog.doFile)
+		{
+			Opener o = new Opener();
+			ImagePlus ip = o.openImage(userDialog.filename);
+			
+			labelImage.initWithIP(ip);
+		}
+		else
+		{
+	//		labelImage.initialGuessGrowing(0.2);
+			labelImage.initialGuessGrowing(0.4);
+	//		
+	//		labelImage.initialGuessEllipsesFromString("4 " +
+	//				"152 79 147 12 1 " +
+	//				"130 91 117 79 2 " +
+	//				"78 18 40 10 3 " +
+	//				"134 107 88 104 4 ");
+		}
+		
+		
+		initStackIP();
+		// first stack image without boundary&contours
+		addSliceToStackAndShow("init", labelImage.getLabelImage().getPixelsCopy());
+		
+
+		// save the initial guess (random/user defindet/whatever) to a tiff
+		// so we can reuse it for debugging
+		boolean doSave=true;
+		if(doSave)
+		{
+//			String s = this.getClass().getProtectionDomain().getCodeSource().getLocation().getPath();
+//			System.out.println(s);
+			String d = originalIP.getOriginalFileInfo().directory;
+			ImagePlus ip = new ImagePlus("", stackImPlus.getProcessor());
+			FileSaver fs = new FileSaver(ip);
+			fs.saveAsTiff(d+"initialLabelImage.tiff");
+		}
+				
 		labelImage.initBoundary();
-//		labelImage.initialGuessGrowing(0.2);
-		
-//		labelImage.initialGuessGrowing(0.4);
-		labelImage.initialGuessRandom();
-		
-//		labelImage.initialGuessEllipsesFromString("4 " +
-//				"152 79 147 12 1 " +
-//				"130 91 117 79 2 " +
-//				"78 18 40 10 3 " +
-//				"134 107 88 104 4 ");
-		
 		labelImage.generateContour();
+		// next stack image is start of algo
 		addSliceToStackAndShow("init", labelImage.getLabelImage().getPixelsCopy());
 
+		IJ.run("3-3-2 RGB"); // stack has to contain at least 2 slices so this LUT applies to all future slices.
+
 		labelImage.GenerateData();
+		
+//		LutLoader lutloader = new LutLoader();
+//		lutloader.run("3-3-2 RGB");
+
+		labelImage.showStatistics();
+//		macroContrast();
 		
 //		new ImagePlus("LabelImage", labelImage.getLabelImage()).show();
 		
 	}
 	
 	
-	void manualSelect()
+	void manualSelect(final LabelImage labelImg)
 	{
-		IJ.showMessage("Select initial guesses (holding shift). press space to process");
+//		IJ.showMessage("Select initial guesses (holding shift). press space to process");
 		
 		KeyListener keyListener = new KeyListener() 
 		{
@@ -125,31 +169,14 @@ public class Region_Competition implements PlugInFilter{
 //				System.out.println("id " + e.getID());
 //				System.out.println("char " + ((int)e.getKeyChar()));
 
-				if(e.getKeyChar() == KeyEvent.VK_SPACE) 
+//				if(e.getKeyChar() == KeyEvent.VK_SPACE) 
 				{
 //					e.consume();
-					Roi roi = originalIP.getRoi();
 					
-					ImageProcessor guessProcessor = new ShortProcessor(originalIP.getWidth(), originalIP.getHeight());
-					ImagePlus guess = new ImagePlus("Guess", guessProcessor);
-					
-//					guess.show();
-					
-//					guess.setRoi(roi);
-					guessProcessor.setValue(1);
-					guessProcessor.fill(roi);
-					
-					// remove keylistener afterwards
-					originalIP.getCanvas().removeKeyListener(this);
-					
-					labelImage = new LabelImage(MVC);
-					labelImage.initWithIP(guess);
-					labelImage.initBoundary();
-					labelImage.generateContour();
-					
-					initStackIP();
-					
-					labelImage.GenerateData();
+					synchronized(labelImg) 
+					{
+						labelImg.notifyAll();
+					}
 				}
 			}
 			@Override
@@ -162,8 +189,51 @@ public class Region_Competition implements PlugInFilter{
 			}
 		};
 		
+		ImageCanvas canvas = originalIP.getCanvas();
 		
-		originalIP.getCanvas().addKeyListener(keyListener);
+		// save old keylisteners, remove them (so we can use all keys to select guess ROIs)
+		KeyListener[] kls = canvas.getKeyListeners();
+		for(KeyListener kl: kls)
+		{
+			canvas.removeKeyListener(kl);
+		}
+		
+		canvas.addKeyListener(keyListener);
+		
+		Roi roi=null;
+		// try to get a ROI from user
+		while(roi==null)
+		{
+			synchronized(labelImg)
+			{
+				try {
+					System.out.println("Waiting for user input (pressing space");
+					labelImg.wait();
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+				System.out.println("end waiting (button was pressed)");
+				
+				roi = originalIP.getRoi();
+				if(roi==null)
+				{
+					IJ.showMessage("No ROI selcted. maybe wrong window");
+				}
+			}
+		}
+		//we have a roi, remove keylistener and reattach the old ones
+		canvas.removeKeyListener(keyListener);
+		for(KeyListener kl: kls)
+		{
+			canvas.addKeyListener(kl);
+		}
+		
+		labelImg.getLabelImage().setValue(1);
+		labelImg.getLabelImage().fill(roi);
+//		labelImg.initBoundary();
+		
 //		originalIP.getWindow().addKeyListener(keyListener);
 //		IJ.getInstance().addKeyListener(keyListener);
 }
@@ -186,18 +256,30 @@ public class Region_Competition implements PlugInFilter{
 		new ImagePlus("LabelImage", labelImage.getLabelImage()).show();
 	}
 	
+	/**
+	 * Initializes the stackIP, putting the originalIP in the first slice
+	 */
 	void initStackIP()
 	{
 		ImageProcessor myIp = originalIP.getProcessor();
+//		ImageProcessor myIp = originalIP.getProcessor().convertToShort(false);
+
+//		stack= originalIP.createEmptyStack();
 		stack = new ImageStack(myIp.getWidth(), myIp.getHeight());
 		stackImPlus = new ImagePlus("Stack of "+originalIP.getTitle(), myIp); 
 		
 		stack.addSlice("original", myIp.convertToShort(false).getPixelsCopy());
-		
+				
 		stackImPlus.setStack(null, stack);
 		stackImPlus.show();
 	}
 	
+/**
+ * Adds a new slice pixels to the end of the stack, 
+ * and sets the new stack position to this slice
+ * @param title		Title of the stack slice
+ * @param pixels	data of the new slice (pixel array)
+ */
 	public void addSliceToStackAndShow(String title, Object pixels)
 	{
 		if(stack==null)
@@ -205,16 +287,29 @@ public class Region_Competition implements PlugInFilter{
 			System.out.println("stack is null");
 			return;
 		}
+//		stackImPlus.getStack().addSlice(title, pixels);
+		
 		stack.addSlice(title, pixels);
 		stackImPlus.setStack(stack);
 		stackImPlus.setPosition(stack.getSize());
+	}
+	
+	/**
+	 * Debug: selects a point in the current image to see it faster in the GUI.
+	 */
+	public void selectPoint(Point p)
+	{
+		//TODO multidim
+//		stackImPlus.setRoi(p.x[0], p.x[1], 1, 1);
+		IJ.makePoint(p.x[0], p.x[1]);
+//		stackImPlus.draw();
 	}
 
 	public ImagePlus getStackImPlus()
 	{
 		return this.stackImPlus;
 	}
-
+	
 	public ImagePlus getOriginalImPlus()
 	{
 		return this.originalIP;
@@ -257,35 +352,36 @@ public class Region_Competition implements PlugInFilter{
 		System.out.println(result);
 	}
 
+	
 	/**
-		 * compares runtime of recursive and explicit statistics
-		 */
-		void testStatistics()
+	 * compares runtime of recursive and explicit statistics
+	 */
+	void testStatistics()
+	{
+		ImagePlus ip = originalIP;
+		ImageProcessor oProc = ip.getChannelProcessor();
+		labelImage = new LabelImage(MVC);
+		labelImage.getLabelImage().insert(oProc, 0, 0);
+		labelImage.initBoundary();
+		labelImage.generateContour();
+		
+		Timer t = new Timer();
+		
+		for(int i=0; i<10; i++)
 		{
-			ImagePlus ip = originalIP;
-			ImageProcessor oProc = ip.getChannelProcessor();
-			labelImage = new LabelImage(MVC);
-			labelImage.getLabelImage().insert(oProc, 0, 0);
-			labelImage.initBoundary();
-			labelImage.generateContour();
+			t.tic(); 
+			labelImage.computeStatistics();
+			long time=t.toc();
+			System.out.println("compute time: "+time);
+//			labelImage.showStatistics();
 			
-			Timer t = new Timer();
-			
-			for(int i=0; i<10; i++)
-			{
-				t.tic(); 
-				labelImage.computeStatistics();
-				long time=t.toc();
-				System.out.println("compute time: "+time);
-	//			labelImage.showStatistics();
-				
-				t.tic();
-				labelImage.renewStatistics();
-				time=t.toc();
-				System.out.println("renew time: "+time);
-	//			labelImage.showStatistics();
-			}
+			t.tic();
+			labelImage.renewStatistics();
+			time=t.toc();
+			System.out.println("renew time: "+time);
+//			labelImage.showStatistics();
 		}
+	}
 
 	/**
 	 * does overwriting of static part in Connectivity work?
@@ -320,9 +416,8 @@ public class Region_Competition implements PlugInFilter{
 		
 		System.out.println("end");
 		
-		
-		
 	}
 
-
 }
+
+
