93ed50e260686d217e5df14b066f6231bb9f6957
Incardona Pietro <incardon@mpi-cbg.de>
2012-10-05 19:36:26 +0200

    new file:   segmentation_bregman/BregmanGLM_.java 	new file:   segmentation_bregman/BregmanGLM_Batch.java 	new file:   segmentation_bregman/bregman/.DS_Store 	new file:   segmentation_bregman/bregman/.svn/all-wcprops 	new file:   segmentation_bregman/bregman/.svn/entries 	new file:   segmentation_bregman/bregman/.svn/text-base/ASplitBregmanSolver.java.svn-base 	new file:   segmentation_bregman/bregman/.svn/text-base/ASplitBregmanSolverTwoRegions.java.svn-base 	new file:   segmentation_bregman/bregman/.svn/text-base/ASplitBregmanSolverTwoRegions3D.java.svn-base 	new file:   segmentation_bregman/bregman/.svn/text-base/ASplitBregmanSolverTwoRegions3DPSF.java.svn-base 	new file:   segmentation_bregman/bregman/.svn/text-base/ASplitBregmanSolverTwoRegionsPSF.java.svn-base 	new file:   segmentation_bregman/bregman/.svn/text-base/Analysis.java.svn-base 	new file:   segmentation_bregman/bregman/.svn/text-base/FindConnectedRegions.java.svn-base 	new file:   segmentation_bregman/bregman/.svn/text-base/GUI.java.svn-base 	new file:   segmentation_bregman/bregman/.svn/text-base/GenericGUI.java.svn-base 	new file:   segmentation_bregman/bregman/.svn/text-base/Headless.java.svn-base 	new file:   segmentation_bregman/bregman/.svn/text-base/MasksDisplay.java.svn-base 	new file:   segmentation_bregman/bregman/.svn/text-base/NRegions.java.svn-base 	new file:   segmentation_bregman/bregman/.svn/text-base/Parameters.java.svn-base 	new file:   segmentation_bregman/bregman/.svn/text-base/Pix.java.svn-base 	new file:   segmentation_bregman/bregman/.svn/text-base/ProjectSimplexSpeed.java.svn-base 	new file:   segmentation_bregman/bregman/.svn/text-base/RegionStatisticsSolver.java.svn-base 	new file:   segmentation_bregman/bregman/.svn/text-base/SingleRegionTask.java.svn-base 	new file:   segmentation_bregman/bregman/.svn/text-base/Tools.java.svn-base 	new file:   segmentation_bregman/bregman/.svn/text-base/TwoRegions.java.svn-base 	new file:   segmentation_bregman/bregman/.svn/text-base/ZoneTask.java.svn-base 	new file:   segmentation_bregman/bregman/.svn/text-base/ZoneTask3D.java.svn-base 	new file:   segmentation_bregman/bregman/ASplitBregmanSolver.java 	new file:   segmentation_bregman/bregman/ASplitBregmanSolverTwoRegions.java 	new file:   segmentation_bregman/bregman/ASplitBregmanSolverTwoRegions3D.java 	new file:   segmentation_bregman/bregman/ASplitBregmanSolverTwoRegions3DPSF.java 	new file:   segmentation_bregman/bregman/ASplitBregmanSolverTwoRegionsPSF.java 	new file:   segmentation_bregman/bregman/AnalysePatch.java 	new file:   segmentation_bregman/bregman/Analysis.java 	new file:   segmentation_bregman/bregman/BLauncher.java 	new file:   segmentation_bregman/bregman/DistanceAnalysis.java 	new file:   segmentation_bregman/bregman/FindConnectedRegions.java 	new file:   segmentation_bregman/bregman/GUI.java 	new file:   segmentation_bregman/bregman/GenericGUI.java 	new file:   segmentation_bregman/bregman/ImagePatches.java 	new file:   segmentation_bregman/bregman/MasksDisplay.java 	new file:   segmentation_bregman/bregman/NRegions.java 	new file:   segmentation_bregman/bregman/ObjectProperties.java 	new file:   segmentation_bregman/bregman/Parameters.java 	new file:   segmentation_bregman/bregman/Pix.java 	new file:   segmentation_bregman/bregman/ProjectSimplexSpeed.java 	new file:   segmentation_bregman/bregman/RegionStatisticsSolver.java 	new file:   segmentation_bregman/bregman/SingleRegionTask.java 	new file:   segmentation_bregman/bregman/Tools.java 	new file:   segmentation_bregman/bregman/TwoRegions.java 	new file:   segmentation_bregman/bregman/ZoneTask.java 	new file:   segmentation_bregman/bregman/ZoneTask3D.java

---
+++ plugin/src/mosaic/segmentation_bregman/BregmanGLM_.java
@@ -0,0 +1,21 @@
+
+//
+//import bregman.GUI;
+//import ij.plugin.PlugIn;
+//
+//
+//
+//public class BregmanGLM_ implements PlugIn {
+//	public void run(String arg0) {
+//		try {
+//			GUI window = new GUI();
+//			window.frame.setVisible(true);
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//	}
+//
+//
+//
+//
+//}
