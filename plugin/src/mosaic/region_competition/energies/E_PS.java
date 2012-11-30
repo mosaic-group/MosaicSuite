2228bd39ff07485aecd3c23fac407cbc5c0d9461
Incardona Pietro <incardon@mpi-cbg.de>
2012-11-30 12:16:25 +0100

    modified:   src/mosaic/region_competition/energies/E_PS.java

--- plugin/src/mosaic/region_competition/energies/E_PS.java
+++ plugin/src/mosaic/region_competition/energies/E_PS.java
@@ -68,24 +68,10 @@ public class E_PS extends ExternalEnergy
 		
     	// read out the size of the mask
     	// vRegion is the size of our temporary window
- //   	int[] vRegion = sphere.getDimensions();
-
-/*		if (contourPoint.x[0] == 474 && contourPoint.x[1] == 54)
-		{
-			int debug = 0;
-			debug++;
-		}*/
     	
     	sphereIt.setMidPoint(contourPoint);
     	
 		// vOffset is basically the difference of the center and the start of the window
-/*		Point vOffset = (new Point(vRegion)).div(2);
-			
-		Point start = contourPoint.sub(vOffset);				// "upper left point"
-		
-		RegionIterator vLabelIt = new RegionIterator(dimensions, vRegion, start.x);
-//		RegionIterator vDataIt = new RegionIterator(dimensions, vRegion, start.x);
-		RegionIteratorMask vMaskIt = new RegionIteratorMask(dimensions, vRegion, start.x);*/
 		
 		double vSumFrom = -value; // we ignore the value of the center point
 		double vSumTo = 0;
@@ -93,18 +79,10 @@ public class E_PS extends ExternalEnergy
 		double vSumOfSqTo = 0.0;
 		int vNFrom = -1;
 		int vNTo = 0;
-
-		final byte[] mask = sphere.mask;
-		final byte fgVal = sphere.fgVal;
-		
-//		List<Integer> DioCane = new ArrayList<Integer>();
-//		List<Integer> DioMaiale = new ArrayList<Integer>();
 		
 		while (sphereIt.hasNext())
 		{
 			int labelIdx = sphereIt.next();
-			
-//			DioCane.add(labelIdx);
 			
 			int dataIdx = labelIdx;
 			int absLabel=labelImage.getLabelAbs(labelIdx);
@@ -123,66 +101,6 @@ public class E_PS extends ExternalEnergy
 				vNTo++;
 			}
 		}
-		
-/*		double vSumFrom = -value; // we ignore the value of the center point
-		double vSumTo = 0;
-		double vSumOfSqFrom = -value * value; // ignore the value of the center point.
-		double vSumOfSqTo = 0.0;
-		int vNFrom = -1;
-		int vNTo = 0;
-		
-		while(vLabelIt.hasNext())
-		{
-			int labelIdx = vLabelIt.next();
-//			int dataIdx = vDataIt.next();
-			int dataIdx = labelIdx;
-			int maskIdx = vMaskIt.next();
-			
-			if(mask[maskIdx]==fgVal)
-			{
-//				DioMaiale.add(labelIdx);
-				
-				int absLabel=labelImage.getLabelAbs(labelIdx);
-				if(absLabel == fromLabel)
-				{
-					double data = intensityImage.get(dataIdx);
-					vSumFrom += data;
-					vSumOfSqFrom += data*data;
-					vNFrom++;
-				}
-				else if(absLabel == toLabel)
-				{
-					double data = intensityImage.get(dataIdx);
-					vSumTo += data;
-					vSumOfSqTo += data*data;
-					vNTo++;
-				}
-			}
-		}
-
-		if (vNTo != vNToS)
-		{
-			int debug = 0;
-			debug++;
-		}
-		
-		if (vNFrom != vNFromS)
-		{
-			int debug = 0;
-			debug++;
-		}
-		
-		if (vSumFrom != vSumFromS)
-		{
-			int debug = 0;
-			debug++;
-		}
-		
-		if (vSumTo != vSumToS)
-		{
-			int debug = 0;
-			debug++;
-		}*/
 		
 		double vMeanTo;
 		double vVarTo;
