f10e928e3593252b97186af4bad309c53f68ff97
Incardona Pietro <incardon@mpi-cbg.de>
2013-12-07 12:01:40 +0100

    Spot detection pligins

---
+++ plugin/src/mosaic/core/particleLinking/linkerOptions.java
@@ -0,0 +1,23 @@
+
+package mosaic.core.particleLinking;
+
+/**
+ * 
+ * Linker options for the particle Linker
+ * 
+ * @author Pietro Incardona
+ *
+ */
+
+
+public class linkerOptions
+{
+	public int linkrange = 1;
+	public float displacement = 10;
+	public float r_sq = 9;
+	public boolean straight_line = false;
+	public boolean force = false;
+	public float l_s = 1.0f;
+	public float l_f = 1.0f;
+	public float l_d = 1.0f;
+}
