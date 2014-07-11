71f8e048d2c68da71a7a4f341b7bfd6d47cac114
Pietro Incardona <incardon@mpi-cbg.de>
2014-07-11 10:35:56 +0200

    Added JUnit test

---
+++ plugin/src/mosaic/bregman/Jtest.java
@@ -0,0 +1,40 @@
+package mosaic.bregman;
+
+import static org.junit.Assert.*;
+import ij.IJ;
+import ij.ImagePlus;
+import io.scif.img.ImgIOException;
+import io.scif.img.ImgOpener;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Vector;
+
+import mosaic.bregman.output.Region3DRScript;
+import mosaic.core.ipc.InterPluginCSV;
+import mosaic.core.utils.ImgTest;
+import mosaic.core.utils.MosaicUtils;
+import mosaic.core.utils.Segmentation;
+import mosaic.core.utils.ShellCommand;
+import mosaic.plugins.BregmanGLM_Batch;
+import mosaic.plugins.Region_Competition;
+import mosaic.region_competition.output.RCOutput;
+import net.imglib2.img.Img;
+
+import org.junit.Test;
+
+public class Jtest 
+{
+	@Test
+	public void segmentation() 
+	{
+		BregmanGLM_Batch BG = new BregmanGLM_Batch();
+		
+		// test the cluster
+		
+		BG.setUseCluster(true);
+		MosaicUtils.<Region3DRScript>testSegmentation(BG,"Squassh_cluster",Region3DRScript.class);
+		
+		MosaicUtils.<Region3DRScript>testSegmentation(BG,"Squassh",Region3DRScript.class);
+	}
+}
---
+++ plugin/src/mosaic/plugins/Jtest.java
@@ -0,0 +1,57 @@
+package mosaic.plugins;
+
+
+import ij.IJ;
+import ij.ImagePlus;
+import ij.plugin.filter.PlugInFilter;
+import ij.process.ImageProcessor;
+
+
+/**
+ * 
+ * This is the set of test 
+ * 
+ * @author Pietro Incardona
+ *
+ */
+
+public class Jtest implements PlugInFilter
+{
+
+	/**
+	 * 
+	 * Run JTest filter
+	 * 
+	 */
+	
+	@Override
+	public void run(ImageProcessor arg0) 
+	{
+				
+	}
+	
+	@Override
+	public int setup(String arg0, ImagePlus arg1) 
+	{
+		mosaic.core.cluster.Jtest jtestMj = new mosaic.core.cluster.Jtest();
+		
+		jtestMj.mergetest();
+		
+		mosaic.core.ipc.Jtest jtestIPC = new mosaic.core.ipc.Jtest();
+		
+		jtestIPC.csvtest();
+		
+		mosaic.region_competition.Jtest jtestRC = new mosaic.region_competition.Jtest();
+		
+		jtestRC.segmentation();
+		
+		mosaic.bregman.Jtest jtestBR = new mosaic.bregman.Jtest();
+		
+		jtestBR.segmentation();
+		
+		IJ.showMessage("All test SUCCEFULLY completed");
+		
+		return DONE;
+	}
+	
+}
