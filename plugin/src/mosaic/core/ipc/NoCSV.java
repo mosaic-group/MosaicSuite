6ddfa722026843ef8a40f28c42bcdf58f6a97e21
Pietro Incardona <incardon@mpi-cbg.de>
2015-03-03 16:09:14 +0100

    Add missing file NoCSV.java

---
+++ plugin/src/mosaic/core/ipc/NoCSV.java
@@ -0,0 +1,14 @@
+package mosaic.core.ipc;
+
+/**
+ * 
+ * No CSV is a stub class for Test plugin when no CSV file are produced
+ * 
+ * @author Pietro Incardona
+ *
+ */
+
+public class NoCSV extends StubProp implements ICSVGeneral
+{
+	
+}
