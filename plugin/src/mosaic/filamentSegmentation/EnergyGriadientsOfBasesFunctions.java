7a99f80e2222cb0ae726109aa00c98491204e2d8
Krzysztof Gonciarz <gonciarz@mpi-cbg.de>
2015-06-25 13:56:24 +0200

    GradientsOfBasesFunctions added together with tests

---
+++ plugin/src/mosaic/filamentSegmentation/EnergyGriadientsOfBasesFunctions.java
@@ -0,0 +1,81 @@
+package mosaic.filamentSegmentation;
+
+import mosaic.math.Matrix;
+
+/**
+ * This class provides energy griadients from B-Spline coeffcients.
+ * Values provided here were precomputed in Matlab and are just copy-pased here.
+ * (see xx_init.m for filament segmentation Matlab code)
+ * 
+ * @author Krzysztof Gonciarz <gonciarz@mpi-cbg.de>
+ */
+public class EnergyGriadientsOfBasesFunctions {
+
+    static Matrix getEnergyGradients(int aCoefficientStep) {
+        Matrix result = null;
+        
+        switch (aCoefficientStep) {
+        case 0: 
+            result = getGradientsFor0();
+            break;
+        case 1: 
+            result = getGradientsFor1();
+            break;
+        case 2: 
+            result = getGradientsFor2();
+            break;
+        case 3: 
+            result = getGradientsFor3();
+            break;
+        case 4: 
+            result = getGradientsFor4();
+            break;
+        default:
+            result = new Matrix(new double [][] {{0}});
+            break;
+                   
+        }
+        return result;
+    }
+    
+    private static Matrix getGradientsFor0() {
+        return new Matrix(new double [][] {{0.1667, 0.6667, 0.1667}});
+    }
+    
+    private static Matrix getGradientsFor1() {
+        return new Matrix(new double [][] {{0.0208, 0.1667, 0.4792, 0.6667, 0.4792,
+                                            0.1667, 0.0208}});
+    }
+    
+    private static Matrix getGradientsFor2() {
+        return new Matrix(new double [][] {{0.0026, 0.0208, 0.0703, 0.1667, 0.3151,
+                                            0.4792, 0.612,  0.6667, 0.612,  0.4792,
+                                            0.3151, 0.1667, 0.0703, 0.0208, 0.0026}});
+    }
+    
+    private static Matrix getGradientsFor3() {
+        return new Matrix(new double [][] {{0.00032552, 0.0026, 0.0088, 0.0208, 0.0407,
+                                            0.0703, 0.1117, 0.1667, 0.236, 0.3151,
+                                            0.3981, 0.4792, 0.5524, 0.612, 0.652,
+                                            0.6667, 0.652,  0.612,  0.5524, 0.4792,
+                                            0.3981, 0.3151, 0.236,  0.1667, 0.1117,
+                                            0.0703, 0.0407, 0.0208, 0.0088, 0.0026,
+                                            0.00032552}});
+    }
+    
+    private static Matrix getGradientsFor4() {
+        return new Matrix(new double [][] {{4.069e-05, 0.00032552, 0.0011, 0.0026, 0.0051,
+                                            0.0088, 0.014, 0.0208, 0.0297, 0.0407, 
+                                            0.0542, 0.0703, 0.0894, 0.1117, 0.1373, 
+                                            0.1667, 0.1997, 0.236, 0.2747, 0.3151, 
+                                            0.3565, 0.3981, 0.4392, 0.4792, 0.5171, 
+                                            0.5524, 0.5843, 0.612, 0.6348, 0.652, 
+                                            0.6629, 0.6667, 0.6629, 0.652, 0.6348, 
+                                            0.612, 0.5843, 0.5524, 0.5171, 0.4792, 
+                                            0.4392, 0.3981, 0.3565, 0.3151, 0.2747, 
+                                            0.236, 0.1997, 0.1667, 0.1373, 0.1117, 
+                                            0.0894, 0.0703, 0.0542, 0.0407, 0.0297, 
+                                            0.0208, 0.014, 0.0088, 0.0051, 0.0026, 
+                                            0.0011, 0.00032552, 4.069e-05}});
+    }
+}
