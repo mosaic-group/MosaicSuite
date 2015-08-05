5ea48ae20351d51de2cf8b25aa15381df1db1957
Krzysztof Gonciarz <gonciarz@mpi-cbg.de>
2015-08-05 11:44:29 +0200

    Cleanup of IAPMosaic_ plugin

--- plugin/src/mosaic/plugins/IAPMosaic_.java
+++ plugin/src/mosaic/plugins/IAPMosaic_.java
@@ -9,68 +9,13 @@ import mosaic.ia.gui.GUIDesign;
  * @author arun.shivanandan@inf.ethz.ch
  * To do: Mask check. for q(d), p(d). ROI. h in q(d).3D
  * sorting array of NN: might be dangerous.
- *
  */
-/**
- * @author arun
- *
- *
- *
- *
- */
-
-
 public class IAPMosaic_ implements PlugIn {
-	
-	
-	 
 	@Override
 	public void run(String arg0) {
-	
-		if (MosaicUtils.checkRequirement() == false)
-			return;
+		if (MosaicUtils.checkRequirement() == false) return;
 		
-		try {
-			GUIDesign window = new GUIDesign();
-			window.frmInteractionAnalysis.setVisible(true);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		
-	    return;
+		GUIDesign window = new GUIDesign();
+		window.frmInteractionAnalysis.setVisible(true);
 	}
-	
-	
-	
-
-	
-	
-	
-	
-	
-
-	
-
-
-		
-	
-	
-
-	
-
-			
-			
-	
-	
-
-
-	
-	
 }
-
-
-
-
-
-
-
