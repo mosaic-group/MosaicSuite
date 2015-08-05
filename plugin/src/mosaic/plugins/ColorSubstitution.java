2ed71c8b330fa5ebffd23d503083875121f91899
Krzysztof Gonciarz <gonciarz@mpi-cbg.de>
2015-08-05 11:11:22 +0200

    Cleanup of ColorSubstitiution

--- plugin/src/mosaic/plugins/ColorSubstitution.java
+++ plugin/src/mosaic/plugins/ColorSubstitution.java
@@ -2,64 +2,64 @@ package mosaic.plugins;
 
 import ij.ImagePlus;
 import ij.gui.GenericDialog;
-import ij.plugin.filter.PlugInFilter;
+import ij.plugin.filter.ExtendedPlugInFilter;
+import ij.plugin.filter.PlugInFilterRunner;
 import ij.process.ImageProcessor;
 import net.imglib2.Cursor;
 import net.imglib2.img.ImagePlusAdapter;
 import net.imglib2.img.Img;
 import net.imglib2.type.NativeType;
 import net.imglib2.type.numeric.RealType;
-import net.imglib2.type.numeric.integer.UnsignedByteType;
 
 
-
-public class ColorSubstitution implements  PlugInFilter
+public class ColorSubstitution implements  ExtendedPlugInFilter
 {
-	<T extends RealType<T> & NativeType<T> > void substitute(ImagePlus imp, double col_from, double col_to)
+	<T extends RealType<T> & NativeType<T> > 
+	void substitute(ImagePlus imp, double col_from, double col_to)
 	{
-		// Color substitution
-		
 		final Img<T> image = ImagePlusAdapter.wrap( imp );
-		
 		Cursor<T> cur = image.cursor();
 		
-		while (cur.hasNext())
-		{
+		while (cur.hasNext()) {
 			cur.next();
-			
-			if (cur.get().getRealFloat() == col_from)
-			{
+			if (cur.get().getRealFloat() == col_from) {
 				cur.get().setReal(col_to);
 			}
 		}
+		
+		imp.repaintWindow();
 	}
 	
 	@Override
 	public int setup(String arg, ImagePlus imp)
 	{
-		GenericDialog gd = new GenericDialog("Color substitution");
-		
-		gd.addNumericField("Color From", 0, 3);
-		gd.addNumericField("Color To", 0, 3);
-
-		gd.showDialog();
-		
-		if (gd.wasCanceled() == true)
-			return DONE;
-		
-		double col_from = gd.getNextNumber();
-		double col_to = gd.getNextNumber();
-		
-		this.<UnsignedByteType>substitute(imp,col_from,col_to);
-		
-		return DONE;
+		return DOES_8G + DOES_16 + DOES_32;
 	}
-	
 	
 	@Override
-	public void run(ImageProcessor ip) 
-	{
-		// TODO Auto-generated method stub
-		
+	public int showDialog(ImagePlus imp, String arg1, PlugInFilterRunner arg2) {
+	    GenericDialog gd = new GenericDialog("Color substitution");
+	    gd.addNumericField("Change color value from:", 0, 3);
+	    gd.addNumericField("                   to:  ", 0, 3);
+	    gd.showDialog();
+	    
+	    if (gd.wasCanceled() == true) return DONE;
+	    
+	    double col_from = gd.getNextNumber();
+	    double col_to = gd.getNextNumber();
+	    
+	    substitute(imp,col_from,col_to);
+	    
+	    return DONE;
 	}
+	
+	@Override
+	public void run(ImageProcessor ip) {
+		// Nothing to do here
+	}
+
+    @Override
+    public void setNPasses(int arg0) {
+        // Nothing to do here
+    }
 }
