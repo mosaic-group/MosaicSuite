e43c5573734c847e0a55298f05ee4569af800873
Krzysztof Gonciarz <gonciarz@mpi-cbg.de>
2015-08-06 11:25:42 +0200

    BesselPSF convolver cleaned up

--- plugin/src/mosaic/plugins/BesselPSF_Convolver.java
+++ plugin/src/mosaic/plugins/BesselPSF_Convolver.java
@@ -3,7 +3,8 @@ package mosaic.plugins;
 import ij.IJ;
 import ij.ImagePlus;
 import ij.gui.GenericDialog;
-import ij.plugin.filter.PlugInFilter;
+import ij.plugin.filter.ExtendedPlugInFilter;
+import ij.plugin.filter.PlugInFilterRunner;
 import ij.process.ImageProcessor;
 
 /**
@@ -20,96 +21,80 @@ import ij.process.ImageProcessor;
  * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. 
  * THE SOFTWARE PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE ETH HAS NO 
  * OBLIGATIONS TO PROVIDE MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.<p>
- *
  */
-public class BesselPSF_Convolver implements PlugInFilter{
 
-	double mXResolution = 1;
+public class BesselPSF_Convolver implements ExtendedPlugInFilter {
+    // Setup stuff + input arguments
+    static final int FLAGS = DOES_8G + DOES_16 + DOES_32;
+    int mSetupFlags = FLAGS;
+    ImagePlus mImp;
+    
+    // User input 
+    double mRMax = .5e-6; // .5um
+    double mNA = 1.2f;
+    double mLambda  = 450*1e-9;
+
+    // Stuff for kernel computing 
+    double mXResolution = 1;
 	double mYResolution = 1;
-	double mRMax = .5e-6; // .5um
-	double mNA = 1.2f;
-	float[] mKernel;
 	int mKernelWidth = 0;
 	int mKernelHeight = 0;
-	double mLambda  = 450*1e-9;
-	//Green wavelength: 490-575 nm
-//	double mLambdaMax = 575*1e-9;
-//	double mLambdaMin = 400*1e-9;
 	
-	
+	@Override
 	public int setup(String aArgs, ImagePlus aImp) {
-		if(aImp == null) {
-			IJ.showMessage("Please open an image.");
-			return DONE;
-		}
-		if(!checkAndSetUnit(aImp)) {
-			return DONE;
-		}
-		if(!showDialog()) {
-			return DONE;
-		}
-		
-		mKernel = generateBesselKernel();
-		
-		return IJ.setupDialog(aImp, DOES_8G + DOES_16 + DOES_32);
+	    mImp = aImp;
+	    mSetupFlags = IJ.setupDialog(aImp, DOES_8G + DOES_16 + DOES_32);
+		return mSetupFlags;
 	}
 	
+	@Override
+	public int showDialog(ImagePlus arg0, String arg1, PlugInFilterRunner arg2) {
+	    if (!checkAndSetUnit(mImp)) {
+	        return DONE;
+	    }
+	    if (!showDialog()) {
+	        return DONE;
+	    }
+	    return mSetupFlags;
+	}
+	
+	@Override
 	public void run(ImageProcessor aImageProcessor) {
-		aImageProcessor.convolve(mKernel, mKernelWidth, mKernelHeight);
+	    float[] kernel = generateBesselKernel();
+		aImageProcessor.convolve(kernel, mKernelWidth, mKernelHeight);
 	}
 	
-	public float[] generateBesselKernel() {
-		int vXRadius = (int)(mRMax / mXResolution) + 1; //in pixel
-		int vYRadius = (int)(mRMax / mYResolution) + 1; //in pixel
-		mKernelWidth = (vXRadius*2+1);
-		mKernelHeight = (vYRadius*2+1);
-		float[] vKernel = new float[mKernelWidth*mKernelHeight];
-//		float[][] vPseudoKernel = new float[mKernelWidth][mKernelHeight];
-		double vBesselMax = bessel_PSF(mXResolution/10, mLambda, mNA); //TODO: What to do(r=0) undefined
-		for(int vYI = 0; vYI < mKernelHeight; vYI++) {
-			for(int vXI = 0; vXI < mKernelWidth; vXI++) {
-				float vDist = (float)Math.sqrt(
-						(vXRadius-vXI)*mXResolution * (vXRadius-vXI)*mXResolution +
-						(vYRadius-vYI)*mYResolution * (vYRadius-vYI)*mYResolution);
-				vKernel[coords(vXI,vYI)] = (float)(bessel_PSF(vDist, mLambda, mNA) / vBesselMax);
-//				vPseudoKernel[vXI][vYI] = (float)(bessel_PSF(vDist, mLambda, mNA) / vBesselMax);
-			}
-		}
-		vKernel[coords(vXRadius,vYRadius)] = 1f;
-//		vPseudoKernel[vXRadius][vYRadius] = 1f;
-//		new ImagePlus("big kernel",new FloatProcessor(vPseudoKernel)).show();
+	private float[] generateBesselKernel() {
+        int vXRadius = (int) (mRMax / mXResolution) + 1; // in pixel
+        int vYRadius = (int) (mRMax / mYResolution) + 1; // in pixel
+        mKernelWidth = (vXRadius * 2 + 1);
+        mKernelHeight = (vYRadius * 2 + 1);
+        float[] vKernel = new float[mKernelWidth * mKernelHeight];
+        
+        // TODO: What to do (r=0 -> undefined)
+        double vBesselMax = bessel_PSF(mXResolution / 10, mLambda, mNA);
+        
+        for (int vYI = 0; vYI < mKernelHeight; vYI++) {
+            for (int vXI = 0; vXI < mKernelWidth; vXI++) {
+                float vDist = (float) Math.sqrt((vXRadius - vXI) * mXResolution * (vXRadius - vXI) * mXResolution
+                        + (vYRadius - vYI) * mYResolution * (vYRadius - vYI) * mYResolution);
+                vKernel[coords(vXI, vYI)] = (float) (bessel_PSF(vDist, mLambda, mNA) / vBesselMax);
+            }
+        }
+        vKernel[coords(vXRadius, vYRadius)] = 1f;
 
-		return vKernel;
-	}
-	
-	public int getKernelWidth() {
-		return mKernelWidth;
-	}
-	public int getKernelHeight() {
-		return mKernelHeight;
-	}
-	/**
-	 * Sets the necessary parameters to generate a kernel using <code>CreateBesselKernel</code>. 
-	 * Use <code>GetKernelWidth</code> to get the width of the kernel (after generation!).
-	 * @param aXResolution The pixel-width of the image(in m).
-	 * @param aYResolution The pixel-height of the image(in m).
-	 * @param aLambda The light wave length (in m).
-	 * @param aNA Numerical aparture of the microscope
-	 * @param vRMax The maximal distance which the kernel supports (for ex. 1e-6 = 1um).
-	 */
-	public void setForAPICall(double aXResolution, double aYResolution, double aLambda, double aNA, double aRMax) 
-	{
-		mXResolution = aXResolution;
-		mYResolution = aYResolution;
-		mLambda = aLambda;
-		mNA = aNA;
-		mRMax = aRMax;
+        return vKernel;
 	}
 	
 	private int coords(int aX, int aY) {
 		return aY * mKernelWidth + aX;
 	}
 	
+	/**
+	 * Gets information from image about its pixel resolution.
+	 * @param aImp Input image
+	 * @return true if setup correctly, false otherwise
+	 */
 	private boolean checkAndSetUnit(ImagePlus aImp) {
 		if(!aImp.getCalibration().getUnit().endsWith("m")){
 			IJ.showMessage("Please set a 'x-meter'(e.g. \"nm\",\"mm\" unit in the image properties.");
@@ -133,7 +118,7 @@ public class BesselPSF_Convolver implements PlugInFilter{
 		if(vUnit.equalsIgnoreCase("mm")) {
 			vS = 1000;
 		}
-		if(vUnit.equalsIgnoreCase(IJ.micronSymbol + "m")) {
+		if(vUnit.equalsIgnoreCase(IJ.micronSymbol + "m") || vUnit.equalsIgnoreCase("um")) {
 			vS = 1000000;
 		}
 		if(vUnit.equalsIgnoreCase("nm")) {
@@ -145,12 +130,42 @@ public class BesselPSF_Convolver implements PlugInFilter{
 			mYResolution = aImp.getCalibration().pixelHeight / vS;
 			return true;
 		}
+		
 		IJ.showMessage("Unit in image properties unknown: " + vUnit +".");
+		
 		return false;
-
 	}
+	
+   private double bessel_PSF(double aRadius, double aLambda, double aApparture) {
+        double vA = 2 * Math.PI * aApparture / aLambda;
+        double vR = 2 * bessel(aRadius * vA, 1, (int)(aRadius * vA + 10))[1] / aRadius;
+        return vR * vR;
+    }
+    
+    private boolean showDialog() {
+        GenericDialog gd = new GenericDialog("Bessel PSF parameter");
+        gd.addNumericField("wavelength", mLambda*1e9, 0, 5, "nm");
+        gd.addNumericField("Numeric Aparture: ", mNA, 0, 5, "");
+        gd.addNumericField("Max Radius",   mRMax*1e9, 0, 5, "nm");
+        gd.showDialog();
+        
+        if (gd.wasCanceled()) return false;
+        
+        mLambda = gd.getNextNumber() * 1e-9;
+        mNA = gd.getNextNumber();
+        mRMax = gd.getNextNumber() * 1e-9;
+        
+        return true;
+    }
+
+    @Override
+    public void setNPasses(int arg0) {
+        // Nothing to be done here
+    }
+	    
+    // ==================================================================================	   
 	/**
-	 * @author � Tao Pang 2006                                                                             
+	 * @author Tao Pang 2006                                                                             
 	 *
 	 *(1) This Java program is part of the book, "An Introduction to        
 	 *Computational Physics, 2nd Edition," written by Tao Pang and      
@@ -158,7 +173,7 @@ public class BesselPSF_Convolver implements PlugInFilter{
 	 *
 	 *(2) No warranties, express or implied, are made for this program.     
 	 */
-	public static double[] bessel(double x, int n, int nb) {
+	private static double[] bessel(double x, int n, int nb) {
 		int nmax = n+nb;
 		double y[] = new double[n+1];
 		double z[] = new double[nmax+1];
@@ -175,24 +190,4 @@ public class BesselPSF_Convolver implements PlugInFilter{
 			y[i] = z[i]/s;
 		return y;
 	}
-	
-	public double bessel_PSF(double aRadius, double aLambda, double aApparture) {
-		double vA = 2 * Math.PI * aApparture / aLambda;
-		double vR = 2 * bessel(aRadius * vA, 1, (int)(aRadius * vA + 10))[1] / aRadius;
-		return vR * vR;
-	}
-	
-	public boolean showDialog() {
-		GenericDialog vGD = new GenericDialog("Bessel PSF parameter");
-		vGD.addNumericField("wavelength", mLambda*1e9, 0, 0, "nm");
-		vGD.addNumericField("Numeric Aparture: ", mNA, 0);
-		vGD.addNumericField("Max Radius", mRMax*1e9, 0, 0, "nm");
-		vGD.showDialog();
-		if (vGD.wasCanceled()) 
-			return false;
-		mLambda = vGD.getNextNumber() * 1e-9;
-		mNA = vGD.getNextNumber();
-		mRMax = vGD.getNextNumber() * 1e-9;
-		return true;
-	}	
 }
