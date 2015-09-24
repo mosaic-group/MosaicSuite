c99cd5a7d691028e50cd4ac62b503c9a2fa24558
Krzysztof Gonciarz <gonciarz@mpi-cbg.de>
2015-09-24 11:46:55 +0200

    particle_tracker (unused) test removed

--- plugin/src/mosaic/plugins/test/Jtest.java
+++ plugin/src/mosaic/plugins/test/Jtest.java
@@ -1,48 +1,40 @@
 package mosaic.plugins.test;
 
 
-import ij.IJ;
-import ij.ImagePlus;
-import ij.plugin.filter.PlugInFilter;
-import ij.process.ImageProcessor;
-
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 
-import mosaic.core.utils.MosaicTest;
-import mosaic.plugins.utils.TimeMeasurement;
-
 import org.apache.log4j.Logger;
 import org.scijava.util.FileUtils;
 
+import ij.ImagePlus;
+import ij.plugin.filter.PlugInFilter;
+import ij.process.ImageProcessor;
+import mosaic.core.utils.MosaicTest;
+import mosaic.plugins.utils.TimeMeasurement;
+
+
 /**
- *
  * This is the set of test
  *
  * @author Pietro Incardona
- *
  */
+public class Jtest implements PlugInFilter {
 
-public class Jtest implements PlugInFilter
-{
     protected static final Logger logger = Logger.getLogger(Jtest.class);
 
     /**
-     *
      * Run JTest filter
-     *
      */
 
     @Override
-    public void run(ImageProcessor arg0)
-    {
+    public void run(ImageProcessor arg0) {
 
     }
 
     @Override
-    public int setup(String arg0, ImagePlus arg1)
-    {
+    public int setup(String arg0, ImagePlus arg1) {
         // Get the User home directory
         String test = MosaicTest.getTestEnvironment();
         File s_file = new File(test + File.separator + "succeful");
@@ -74,26 +66,15 @@ public class Jtest implements PlugInFilter
         jtestRC.segmentation();
         tm.logLapTimeSec("========================== region_competition.Jtest");
 
-        // Tracker
-        // TODO: Not working. Seems that test data are not complete (mandatory .cfg file is missing)
-        //		logger.info("========================== TestSuite: ParticleTracker3DModular_ ===================================");
-        //	    ParticleTracker3DModular_ pt = new ParticleTracker3DModular_();
-        //	    MosaicTest.<Particle>testPlugin(pt,"particle_tracker",Particle.class);
-
-
         logger.info("All tests SUCCESSFULLY completed");
 
-        IJ.showMessage("All test SUCCEFULLY completed");
         // Create a file that notify all test has been completed suceffuly
-
-        try
-        {
+        try {
             PrintWriter succeful = new PrintWriter(test + File.separator + "succeful");
             succeful.write(1);
             succeful.close();
         }
-        catch (FileNotFoundException e)
-        {
+        catch (FileNotFoundException e) {
             e.printStackTrace();
         }
 
