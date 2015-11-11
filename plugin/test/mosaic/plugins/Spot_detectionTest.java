851d2febb83bacfd2ce0869070cf9856b0ff2b0d
Krzysztof Gonciarz <gonciarz@mpi-cbg.de>
2015-11-11 15:38:11 +0100

    Some more tests added for spot detection

--- plugin/test/mosaic/plugins/Spot_detectionTest.java
+++ plugin/test/mosaic/plugins/Spot_detectionTest.java
@@ -29,5 +29,50 @@ public class Spot_detectionTest extends CommonBase {
                    expectedCsvFiles, referenceCsvFiles,
                    setupString, macroOptions);
     }
+    
+    @Test
+    public void testThreeFrames()  {
+        // Define test data
+        final String tcDirName          = "FeaturePointDetection/threeFrames/";
+        final String setupString        = "run";
+        final String macroOptions       = "radius=3 cutoff=0.0 per/abs=0.4";
+        final String[] inputFiles       = {"threeFrames.tif"};
+        final String[] expectedFiles    = {};
+        final String[] referenceFiles   = {};
+        final String[] expectedCsvFiles    = {"threeFrames.tifdet.csv"};
+        final String[] referenceCsvFiles   = {"threeFrames.csv"};
 
+        // Create tested plugIn
+        final Spot_detection nt = new Spot_detection();
+
+        // Test it
+        testPlugin(nt, tcDirName,
+                   inputFiles, 
+                   expectedFiles, referenceFiles,
+                   expectedCsvFiles, referenceCsvFiles,
+                   setupString, macroOptions);
+    }
+
+    @Test
+    public void test3D()  {
+        // Define test data
+        final String tcDirName          = "FeaturePointDetection/3d/";
+        final String setupString        = "run";
+        final String macroOptions       = "radius=3 cutoff=3 per/abs=0.1";
+        final String[] inputFiles       = {"3Dframe.tif"};
+        final String[] expectedFiles    = {};
+        final String[] referenceFiles   = {};
+        final String[] expectedCsvFiles    = {"3Dframe.tifdet.csv"};
+        final String[] referenceCsvFiles   = {"3Dframe.csv"};
+
+        // Create tested plugIn
+        final Spot_detection nt = new Spot_detection();
+
+        // Test it
+        testPlugin(nt, tcDirName,
+                   inputFiles, 
+                   expectedFiles, referenceFiles,
+                   expectedCsvFiles, referenceCsvFiles,
+                   setupString, macroOptions);
+    }
 }
