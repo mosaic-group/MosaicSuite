e5f68abc87012089cc16c15edcb589eae35c074b
Krzysztof Gonciarz <gonciarz@mpi-cbg.de>
2015-11-20 12:10:25 +0100

    Added two more junit tests for squassh

--- plugin/test/mosaic/plugins/BregmanGLM_BatchTest.java
+++ plugin/test/mosaic/plugins/BregmanGLM_BatchTest.java
@@ -33,6 +33,33 @@ public class BregmanGLM_BatchTest extends CommonBase{
     }
 
     @Test
+    public void testTest2dFull()  {
+        
+        // Define test data
+        final String tcDirName           = "Squassh/Test2dFull/";
+        final String setupString         = "run";
+        final String macroOptions        = "";
+        final String inputFile           = "test2d.tif";
+        final String[] expectedImgFiles  = {"__outline_overlay_c1.zip/test2d_outline_overlay_c1.zip", "__intensities_c1.zip/test2d_intensities_c1.zip",
+                                            "__mask_c1.zip/test2d_mask_c1.zip", "__seg_c1.zip/test2d_seg_c1.zip"};
+        final String[] referenceImgFiles = {"__outline_overlay_c1.zip/test2d_outline_overlay_c1.zip", "__intensities_c1.zip/test2d_intensities_c1.zip",
+                                            "__mask_c1.zip/test2d_mask_c1.zip", "__seg_c1.zip/test2d_seg_c1.zip"};
+        final String[] expectedFiles     = {"__ObjectsData_c1.csv/test2d_ObjectsData_c1.csv", "__ImagesData.csv/test2d_ImagesData.csv"};
+        final String[] referenceFiles    = {"__ObjectsData_c1.csv/test2d_ObjectsData_c1.csv", "__ImagesData.csv/test2d_ImagesData.csv"};
+
+        // Create tested plugIn
+        final BregmanGLM_Batch plugin = new BregmanGLM_Batch();
+        copyTestResources("spb_settings.dat", SystemOperations.getTestDataPath() + tcDirName, "/tmp");
+        
+        // Test it
+        testPlugin(plugin, tcDirName,
+                   macroOptions, 
+                   setupString, inputFile,
+                   expectedImgFiles, referenceImgFiles,
+                   expectedFiles, referenceFiles);
+    }
+    
+    @Test
     public void testSphereSmall3D()  {
         
         // Define test data
@@ -44,6 +71,31 @@ public class BregmanGLM_BatchTest extends CommonBase{
         final String[] referenceImgFiles = {"__outline_overlay_c1.zip/sphereSmall_outline_overlay_c1.zip"};
         final String[] expectedFiles     = {"__ObjectsData_c1.csv/sphereSmall_ObjectsData_c1.csv", "__ImagesData.csv/sphereSmall_ImagesData.csv"};
         final String[] referenceFiles    = {"__ObjectsData_c1.csv/sphereSmall_ObjectsData_c1.csv", "__ImagesData.csv/sphereSmall_ImagesData.csv"};
+
+        // Create tested plugIn
+        final BregmanGLM_Batch plugin = new BregmanGLM_Batch();
+        copyTestResources("spb_settings.dat", SystemOperations.getTestDataPath() + tcDirName, "/tmp");
+        
+        // Test it
+        testPlugin(plugin, tcDirName,
+                   macroOptions, 
+                   setupString, inputFile,
+                   expectedImgFiles, referenceImgFiles,
+                   expectedFiles, referenceFiles);
+    }
+    
+    @Test
+    public void testVideo3D()  {
+        
+        // Define test data
+        final String tcDirName           = "Squassh/Video3D/";
+        final String setupString         = "run";
+        final String macroOptions        = "";
+        final String inputFile           = "dropletVideoSmall.tif";
+        final String[] expectedImgFiles  = {"__outline_overlay_c1.zip/dropletVideoSmall_outline_overlay_c1.zip"};
+        final String[] referenceImgFiles = {"__outline_overlay_c1.zip/dropletVideoSmall_outline_overlay_c1.zip"};
+        final String[] expectedFiles     = {"__ObjectsData_c1.csv/dropletVideoSmall_ObjectsData_c1.csv", "__ImagesData.csv/dropletVideoSmall_ImagesData.csv"};
+        final String[] referenceFiles    = {"__ObjectsData_c1.csv/dropletVideoSmall_ObjectsData_c1.csv", "__ImagesData.csv/dropletVideoSmall_ImagesData.csv"};
 
         // Create tested plugIn
         final BregmanGLM_Batch plugin = new BregmanGLM_Batch();
