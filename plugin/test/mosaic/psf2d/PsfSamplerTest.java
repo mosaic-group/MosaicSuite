f51cf90fb78ac4edd288a3e151c71c0897786e70
gonciarz <gonciarz@sbalzarini-mac-3-wifi.mpi-cbg.de>
2016-01-07 12:35:12 +0100

    Tests for PsfSampler updated

--- plugin/test/mosaic/psf2d/PsfSamplerTest.java
+++ plugin/test/mosaic/psf2d/PsfSamplerTest.java
@@ -9,20 +9,15 @@ import ij.process.ByteProcessor;
 public class PsfSamplerTest {
 
     @Test
-    public void testBlackBox() {
-        /*
-         * This test just check current behavior of PsfPointSpreadFunction. Output is check as "it is" and 
-         * it is not validated if this is corrected or not. Main purpose of this test is to help to refactor code
-         * and prevent from introducing unwanted changes. 
-         */
-        final byte[] originalImgArray2 = {  0, 0, 0, 0, 0,
-                                            0, 0, 0, 0, 0,
-                                            0, 1, 8, 1, 0,
-                                            0, 8, 61,8, 0,
-                                            0, 1, 8, 1, 0,
-                                            0, 0, 0, 0, 0,
-                                            0, 0, 0, 0, 0};
-        final ByteProcessor fp = new ByteProcessor(5, 7, originalImgArray2);
+    public void testPsfSampler() {
+        final byte[] originalImgArray = {  0, 0, 0, 0, 0,
+                                           0, 0, 0, 0, 0,
+                                           0, 1, 8, 1, 0,
+                                           0, 8, 61,8, 0,
+                                           0, 1, 8, 1, 0,
+                                           0, 0, 0, 0, 0,
+                                           0, 0, 0, 0, 0};
+        final ByteProcessor fp = new ByteProcessor(5, 7, originalImgArray);
         
         PsfSampler psf = new PsfSampler(fp, new PsfSourcePosition(2, 3), 2, 8, 3, 1, 1);
         float[] result = psf.getPsf();
