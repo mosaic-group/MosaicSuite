f94f14ce00949bb66bafc637482592607cfdc1d0
gonciarz <gonciarz@sbalzarini-mac-3-wifi.mpi-cbg.de>
2016-01-11 17:32:37 +0100

    Cleanup of Pearson (first part) + tests

--- plugin/src/mosaic/bregman/Pearson.java
+++ plugin/src/mosaic/bregman/Pearson.java
@@ -7,76 +7,54 @@ import ij.ImageStack;
 import ij.process.ByteProcessor;
 import ij.process.ImageProcessor;
 
-
 class Pearson {
-
-    private final int width, height, nbSlices, depth;
-    private boolean[][][] cellmaskA;
-    private boolean[][][] cellmaskB;
     private final int ni, nj, nz;
-    private final Parameters p;
-    private int osz;
-    private double sumA, sumB, sumAB, sumsqrA, Aarraymean, Barraymean;
-    private double maxA, maxB;
     private final double[][][] imageA;
     private final double[][][] imageB;
+    private boolean[][][] cellmaskA;
+    private boolean[][][] cellmaskB;
 
-    public Pearson(ImagePlus ipA, ImagePlus ipB, Parameters p) {
-        this.p = p;
+    private final Parameters p;
+
+    public Pearson(ImagePlus ipA, ImagePlus ipB, Parameters par) {
+        this.p = par;
         this.ni = p.ni;
         this.nj = p.nj;
         this.nz = p.nz;
-        this.width = ipA.getWidth();
-        this.height = ipA.getHeight();
-        this.nbSlices = ipA.getNSlices();
-        this.depth = ipA.getBitDepth();
+        
+        if (ipA.getWidth() != ipB.getWidth() || ipA.getHeight() != ipB.getHeight() || ipA.getNSlices() != ipB.getNSlices() || ipA.getBitDepth() != ipB.getBitDepth()) {
+            IJ.error("ImageColocalizer expects both images to have the same size and depth");
+            throw new RuntimeException();
+        }
+        
+        int osz = (p.nz > 1) ? p.model_oversampling : 1;
 
         imageA = new double[nz][ni][nj];
-        imageB = new double[nz][ni][nj];
-
-        if (this.width != ipB.getWidth() || this.height != ipB.getHeight() || this.nbSlices != ipB.getNSlices() || this.depth != ipB.getBitDepth()) {
-            IJ.error("ImageColocalizer expects both images to have the same size and depth");
-            return;
-        }
-
-        if (p.nz > 1) {
-            osz = p.model_oversampling;
-        }
-        else {
-            osz = 1;
-        }
-        final int os = p.model_oversampling;
-
-        for (int z = 0; z < nz / osz; z++) {
-            ipA.setSlice(z + 1);
-            ImageProcessor imp = ipA.getProcessor();
-            for (int i = 0; i < ni / os; i++) {
-                for (int j = 0; j < nj / os; j++) {
-                    imageA[z][i][j] = imp.getPixel(i, j);
-                    if (imp.getPixel(i, j) > maxA) {
-                        maxA = imp.getPixel(i, j);
-                    }
-                }
-            }
-        }
-
-        for (int z = 0; z < nz / osz; z++) {
-            ipB.setSlice(z + 1);
-            ImageProcessor imp = ipB.getProcessor();
-            for (int i = 0; i < ni / os; i++) {
-                for (int j = 0; j < nj / os; j++) {
-                    imageB[z][i][j] = imp.getPixel(i, j);
-                    if (imp.getPixel(i, j) > maxB) {
-                        maxB = imp.getPixel(i, j);
-                    }
-                }
-            }
-        }
-
-        double tx = (Analysis.p.usecellmaskX) ? Analysis.p.thresholdcellmask * maxA : 0; 
-        double ty = (Analysis.p.usecellmaskY) ? Analysis.p.thresholdcellmasky * maxB : 0;
+        double maxA = initImageAndGetMax(ipA, imageA, osz);
+        double tx = (p.usecellmaskX) ? p.thresholdcellmask * maxA : 0; 
         cellmaskA = createBinaryCellMask(tx, ipA, 0, osz);
+        
+        imageB = new double[nz][ni][nj];
+        double maxB = initImageAndGetMax(ipB, imageB, osz);
+        double ty = (p.usecellmaskY) ? p.thresholdcellmasky * maxB : 0;
         cellmaskB = createBinaryCellMask(ty, ipB, 1, osz);
+    }
+
+    private double initImageAndGetMax(ImagePlus inputImage, double[][][] image, int osz) {
+        double max = 0;
+        for (int z = 0; z < nz / osz; z++) {
+            inputImage.setSlice(z + 1);
+            ImageProcessor imp = inputImage.getProcessor();
+            for (int i = 0; i < ni / p.model_oversampling; i++) {
+                for (int j = 0; j < nj / p.model_oversampling; j++) {
+                    image[z][i][j] = imp.getPixelValue(i, j);
+                    if (imp.getPixelValue(i, j) > max) {
+                        max = imp.getPixelValue(i, j);
+                    }
+                }
+            }
+        }
+        return max;
     }
 
     public double[] run() {
@@ -90,73 +68,49 @@ class Pearson {
     }
 
     private double[] linreg(double[][][] Aarray, double[][][] Barray, int TA, int TB, int mask) {// mask == 0 no mask, 1 : mask, 2 : outside zet to zero only
+        double sumA = 0;
+        double sumB = 0;
+        double sumAB = 0;
+        double sumsqrA = 0;
+        int count = 0;
+
+        for (int z = 0; z < nz; z++) {
+            for (int i = 0; i < ni; i++) {
+                for (int j = 0; j < nj; j++) {
+                    boolean bothMaskSet = cellmaskB[z][i][j] && cellmaskA[z][i][j];
+                    final boolean bothArraysSet = Aarray[z][i][j]>=TA && Barray[z][i][j]>=TB;
+                    if (bothArraysSet && (mask == 0 || bothMaskSet)) {
+                        sumA+=Aarray[z][i][j];
+                        sumB+=Barray[z][i][j];
+                        sumAB+=Aarray[z][i][j]*Barray[z][i][j];
+                        sumsqrA+=Math.pow(Aarray[z][i][j],2);
+                    }
+                    count++;    
+                }
+            }
+        }
+
+        double Aarraymean = sumA / count;
+        double Barraymean = sumB / count;
+
         double num = 0;
         double den1 = 0;
         double den2 = 0;
+        for (int z = 0; z < nz; z++) {
+            for (int i = 0; i < ni; i++) {
+                for (int j = 0; j < nj; j++) {
+                    boolean bothMaskSet = cellmaskB[z][i][j] && cellmaskA[z][i][j];
+                    final boolean bothArraysSet = Aarray[z][i][j]>=TA && Barray[z][i][j]>=TB;
+                    if (bothArraysSet && (mask == 0 || bothMaskSet)) {
+                        num += (Aarray[z][i][j] - Aarraymean) * (Barray[z][i][j] - Barraymean);
+                        den1 += Math.pow((Aarray[z][i][j] - Aarraymean), 2);
+                        den2 += Math.pow((Barray[z][i][j] - Barraymean), 2);
+                    }
+                }
+            }
+        }
+
         final double[] coeff = new double[6];
-        int count = 0;
-
-        sumA = 0;
-        sumB = 0;
-        sumAB = 0;
-        sumsqrA = 0;
-        Aarraymean = 0;
-        Barraymean = 0;
-
-        for (int z = 0; z < nz; z++) {
-            for (int i = 0; i < ni; i++) {
-                for (int j = 0; j < nj; j++) {
-                    boolean cond;
-                    if (mask == 1) {
-                        cond = (Aarray[z][i][j] >= TA && Barray[z][i][j] >= TB && cellmaskB[z][i][j] && cellmaskA[z][i][j]);
-                    }
-                    else {
-                        cond = (Aarray[z][i][j] >= TA && Barray[z][i][j] >= TB);
-                    }
-                    if (cond) {
-                        if (mask == 2 && (!cellmaskB[z][i][j] || !cellmaskA[z][i][j])) {
-                            count++;
-                        }
-                        else {
-                            sumA += Aarray[z][i][j];
-                            sumB += Barray[z][i][j];
-                            sumAB += Aarray[z][i][j] * Barray[z][i][j];
-                            sumsqrA += Math.pow(Aarray[z][i][j], 2);
-                            count++;
-                        }
-                    }
-
-                }
-            }
-        }
-
-        Aarraymean = sumA / count;
-        Barraymean = sumB / count;
-
-        for (int z = 0; z < nz; z++) {
-            for (int i = 0; i < ni; i++) {
-                for (int j = 0; j < nj; j++) {
-                    boolean cond;
-                    if (mask == 1) {
-                        cond = (Aarray[z][i][j] >= TA && Barray[z][i][j] >= TB && cellmaskB[z][i][j] && cellmaskA[z][i][j]);
-                    }
-                    else {
-                        cond = (Aarray[z][i][j] >= TA && Barray[z][i][j] >= TB);
-                    }
-                    if (cond) {
-                        if (mask == 2 && (!cellmaskB[z][i][j] || !cellmaskA[z][i][j])) {
-
-                        }
-                        else {
-                            num += (Aarray[z][i][j] - Aarraymean) * (Barray[z][i][j] - Barraymean);
-                            den1 += Math.pow((Aarray[z][i][j] - Aarraymean), 2);
-                            den2 += Math.pow((Barray[z][i][j] - Barraymean), 2);
-                        }
-                    }
-                }
-            }
-        }
-
         // 0:a, 1:b, 2:corr coeff, 3: num, 4: den1, 5: den2
         coeff[0] = (count * sumAB - sumA * sumB) / (count * sumsqrA - Math.pow(sumA, 2));
         coeff[1] = (sumsqrA * sumB - sumA * sumAB) / (count * sumsqrA - Math.pow(sumA, 2));
@@ -182,17 +136,14 @@ class Pearson {
             return cellmask;
         }
 
-        final ImagePlus maska_im = new ImagePlus();
         final ImageStack maska_ims = new ImageStack(ni, nj);
-        ImageProcessor imp;
-
         for (int z = 0; z < nz; z++) {
             img.setSlice(z / osz + 1);
-            imp = img.getProcessor();
+            ImageProcessor imp = img.getProcessor();
             final byte[] maska_bytes = new byte[ni * nj];
             for (int i = 0; i < ni; i++) {
                 for (int j = 0; j < nj; j++) {
-                    if (imp.getPixel(i / p.model_oversampling, j / p.model_oversampling) > threshold) {
+                    if (imp.getPixelValue(i / p.model_oversampling, j / p.model_oversampling) > threshold) {
                         maska_bytes[j * ni + i] = (byte) 255;
                     }
                     else {
@@ -206,8 +157,8 @@ class Pearson {
             maska_ims.addSlice("", bp);
         }
 
+        final ImagePlus maska_im = new ImagePlus();
         maska_im.setStack("Cell mask channel " + (channel + 1), maska_ims);
-
         IJ.run(maska_im, "Invert", "stack");
         IJ.run(maska_im, "Fill Holes", "stack");
         IJ.run(maska_im, "Open", "stack");
@@ -215,10 +166,10 @@ class Pearson {
 
         for (int z = 0; z < nz; z++) {
             maska_im.setSlice(z + 1);
-            imp = maska_im.getProcessor();
+            ImageProcessor imp = maska_im.getProcessor();
             for (int i = 0; i < ni; i++) {
                 for (int j = 0; j < nj; j++) {
-                    cellmask[z][i][j] = imp.getPixel(i, j) > 254;
+                    cellmask[z][i][j] = imp.getPixelValue(i, j) != 0;
                 }
             }
         }
---
+++ plugin/test/mosaic/bregman/PearsonTest.java
@@ -0,0 +1,162 @@
+package mosaic.bregman;
+
+import static org.junit.Assert.assertArrayEquals;
+
+import java.util.Arrays;
+
+import org.junit.Test;
+
+import ij.ImagePlus;
+import ij.ImageStack;
+import ij.process.FloatProcessor;
+
+
+public class PearsonTest {
+
+    @Test
+    public void test1_2D() {
+        final float[][] imgA = {{1, 0, 0, 0, 0},
+                                {0, 1, 0, 0, 0},
+                                {0, 0, 1, 0, 0},
+                                {0, 0, 0, 1, 0},
+                                {0, 0, 0, 0, 1}};
+        ImagePlus ipA = new ImagePlus("", new FloatProcessor(imgA));
+        final float[][] imgB = {{0, 0, 0, 0, 1},
+                                {0, 0, 0, 1, 0},
+                                {0, 0, 1, 0, 0},
+                                {0, 1, 0, 0, 0},
+                                {1, 0, 0, 0, 0}};
+        ImagePlus ipB = new ImagePlus("", new FloatProcessor(imgB));
+        Parameters parameters = new Parameters();
+        parameters.ni = 5;
+        parameters.nj = 5;
+        parameters.nz = 1;
+        parameters.model_oversampling = 1;
+        
+        double[] result = new Pearson(ipA, ipB, parameters).run();
+        assertArrayEquals(new double[]{0, 0, 0}, result, 1e-6);
+    }
+    
+    @Test
+    public void test2_2D() {
+        final float[][] imgA = {{0, 0, 0, 0, 1},
+                                {0, 0, 0, 1, 0},
+                                {0, 0, 1, 0, 0},
+                                {0, 1, 0, 0, 0},
+                                {1, 0, 0, 0, 0}};
+        ImagePlus ipA = new ImagePlus("", new FloatProcessor(imgA));
+        final float[][] imgB = {{0, 0, 0, 0, 1},
+                                {0, 0, 0, 1, 0},
+                                {0, 0, 1, 0, 0},
+                                {0, 1, 0, 0, 0},
+                                {1, 0, 0, 0, 0}};
+        ImagePlus ipB = new ImagePlus("", new FloatProcessor(imgB));
+        Parameters parameters = new Parameters();
+        parameters.ni = 5;
+        parameters.nj = 5;
+        parameters.nz = 1;
+        parameters.model_oversampling = 1;
+        
+        double[] result = new Pearson(ipA, ipB, parameters).run();
+        assertArrayEquals(new double[]{1, 1, 1}, result, 1e-6);
+    }
+    
+    @Test
+    public void test3_2D() {
+        final float[][] imgA = {{0, 0, 0, 0, 0},
+                                {0, 1, 1, 1, 0},
+                                {0, 1, 0, 1, 0},
+                                {0, 1, 1, 1, 0},
+                                {0, 0, 0, 0, 0}};
+        ImagePlus ipA = new ImagePlus("", new FloatProcessor(imgA));
+        final float[][] imgB = {{0, 0, 0, 0, 0},
+                                {0, 1, 1, 1, 0},
+                                {0, 1, 0, 1, 0},
+                                {0, 1, 1, 1, 0},
+                                {0, 0, 0, 0, 0}};
+        ImagePlus ipB = new ImagePlus("", new FloatProcessor(imgB));
+        Parameters parameters = new Parameters();
+        parameters.ni = 5;
+        parameters.nj = 5;
+        parameters.nz = 1;
+        parameters.model_oversampling = 1;
+        parameters.usecellmaskY = true;
+        parameters.thresholdcellmasky = 0.5;
+        parameters.usecellmaskX = true;
+        parameters.thresholdcellmask = 0.25;
+        
+        double[] result = new Pearson(ipA, ipB, parameters).run();
+        assertArrayEquals(new double[]{1, 1, 1}, result, 1e-6);
+    }
+    
+    @Test
+    public void test1_3D() {
+        final float[][][] imgA = {{{1, 0, 0},
+                                   {0, 0, 0},
+                                   {0, 0, 0}},
+                
+                                  {{0, 0, 0},
+                                   {0, 1, 0},
+                                   {0, 0, 0}},
+                                  
+                                  {{0, 0, 0},
+                                   {0, 0, 0},
+                                   {0, 0, 1}} };
+        ImageStack is = new ImageStack(3, 3);
+        for (int i = 0; i < 3; i++) is.addSlice(new FloatProcessor(imgA[i]));
+        ImagePlus ipA = new ImagePlus("", is);
+        ImagePlus ipB = new ImagePlus("", is);
+        
+        Parameters parameters = new Parameters();
+        parameters.ni = 3;
+        parameters.nj = 3;
+        parameters.nz = 3;
+        parameters.model_oversampling = 1;
+        
+        double[] result = new Pearson(ipA, ipB, parameters).run();
+        assertArrayEquals(new double[]{1, 1, 1}, result, 1e-6);
+    }
+    
+    @Test
+    public void test2_3D() {
+        final float[][][] imgA = {{{0, 0, 0},
+                                   {0, 0, 0},
+                                   {1, 0, 0}},
+                
+                                  {{0, 0, 0},
+                                   {0, 1, 0},
+                                   {0, 0, 0}},
+                                  
+                                  {{0, 0, 1},
+                                   {0, 0, 0},
+                                   {0, 0, 0}} };
+        ImageStack isa = new ImageStack(3, 3);
+        for (int i = 0; i < 3; i++) isa.addSlice(new FloatProcessor(imgA[i]));
+        ImagePlus ipA = new ImagePlus("", isa);
+        final float[][][] imgB =  {{{0, 0, 1},
+                                    {0, 0, 0},
+                                    {0, 0, 0}},
+                        
+                                   {{0, 0, 0},
+                                    {0, 1, 0},
+                                    {0, 0, 0}},
+                                   
+                                   {{0, 0, 0},
+                                    {0, 0, 0},
+                                    {1, 0, 0}} };
+        ImageStack isb = new ImageStack(3, 3);
+        for (int i = 0; i < 3; i++) isb.addSlice(new FloatProcessor(imgB[i]));
+        ImagePlus ipB = new ImagePlus("", isb);
+        
+        Parameters parameters = new Parameters();
+        parameters.ni = 3;
+        parameters.nj = 3;
+        parameters.nz = 3;
+        parameters.model_oversampling = 1;
+        
+        double[] result = new Pearson(ipA, ipB, parameters).run();
+        assertArrayEquals(new double[]{0.25, 0.25, 0.25}, result, 1e-6);
+  
+    }
+    
+}
