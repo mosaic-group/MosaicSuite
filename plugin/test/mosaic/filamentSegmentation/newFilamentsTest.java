35ba901df56ce27de6136766ddaa2287b15d7732
Krzysztof Gonciarz <gonciarz@mpi-cbg.de>
2016-04-29 12:55:52 +0200

    More utils for graphs

--- plugin/test/mosaic/filamentSegmentation/newFilamentsTest.java
+++ plugin/test/mosaic/filamentSegmentation/newFilamentsTest.java
@@ -40,6 +40,8 @@ import mosaic.test.framework.CommonBase;
 import mosaic.utils.ConvertArray;
 import mosaic.utils.ImgUtils;
 import mosaic.utils.math.CubicSmoothingSpline;
+import mosaic.utils.math.GraphUtils;
+import mosaic.utils.math.GraphUtils.IntVertex;
 import mosaic.utils.math.Matlab;
 import mosaic.utils.math.Matrix;
 import mosaic.utils.math.Matrix.MFunc;
@@ -47,192 +49,64 @@ import mosaic.utils.math.Matrix.MFunc;
 
 public class newFilamentsTest extends CommonBase {
     private static final Logger logger = Logger.getLogger(newFilamentsTest.class);
-    @Test
-    public void testGraph() {
-        Matrix img = new Matrix(new double[][] { { 0, 1, 1, 0, 0 }, 
-                                                 { 0, 0, 0, 1, 0 }, 
-                                                 { 0, 1, 1, 1, 0 }, 
-                                                 { 0, 0, 0, 1, 1 } });
-        
-        List<List<Integer>> conn = findAllElementsOfObject(img, true);
-        logger.debug(conn);
-        UndirectedGraph<Vertex, DefaultEdge> g = new SimpleGraph<>(DefaultEdge.class);
-
-        for (int i = 0; i < conn.size(); ++i) {
-            int idx = conn.get(i).get(0);
-            Vertex v = new Vertex(idx);
-            g.addVertex(v);
-
-            for (int z = 1; z < conn.get(i).size(); z++) {
-                int idxz = conn.get(i).get(z);
-                Vertex vz = new Vertex(idxz);
-                g.addVertex(vz);
-                g.addEdge(v, vz);
-                logger.debug(v + "  ---  " + vz);
-            }
-        }
-
-        logger.debug("FULL: " + g);
-         KruskalMinimumSpanningTree<Vertex, DefaultEdge> mst = new KruskalMinimumSpanningTree<>(g);
-         UndirectedGraph<Vertex, DefaultEdge> gMst = new SimpleGraph<>(DefaultEdge.class);
-         for (Vertex v : g.vertexSet()) gMst.addVertex(v);
-         for (DefaultEdge e : mst.getEdgeSet()) gMst.addEdge(g.getEdgeSource(e), g.getEdgeTarget(e));
-
-         logger.debug(" MST: " + gMst);
-         gMst = g;
-         WeightedGraph<Vertex, DefaultWeightedEdge> gs = new SimpleWeightedGraph<>(DefaultWeightedEdge.class);
-         for (Vertex v : gMst.vertexSet()) gs.addVertex(v);
-         for (DefaultEdge e : gMst.edgeSet()) {
-             DefaultWeightedEdge edge = gs.addEdge(gMst.getEdgeSource(e), gMst.getEdgeTarget(e));
-             gs.setEdgeWeight(edge, 1);
-         }
-         boolean done = false;
-         while (!done) {
-             done = true;
-             for (Vertex v  : gs.vertexSet()) {
-                 Set<DefaultWeightedEdge> edges = gs.edgesOf(v);
-                 logger.debug("Processing v: " + v);
-                 if (edges.size() == 2) {
-                     DefaultWeightedEdge[] e = edges.toArray(new DefaultWeightedEdge[0]);
-                     double w1 = gs.getEdgeWeight(e[0]);
-                     double w2 = gs.getEdgeWeight(e[1]);
-                     Vertex es1 = gs.getEdgeSource(e[0]);
-                     Vertex et1 = gs.getEdgeTarget(e[0]);
-                     Vertex es2 = gs.getEdgeSource(e[1]);
-                     Vertex et2 = gs.getEdgeTarget(e[1]);
-                     Vertex ns = (es1.equals(v)) ? et1 : es1;
-                     Vertex nt = (es2.equals(v)) ? et2 : es2;
-                     if (gs.getEdge(ns, nt) != null) continue;
-                     gs.removeVertex(v);
-                     DefaultWeightedEdge newEdge = gs.addEdge(ns, nt);
-                     gs.setEdgeWeight(newEdge, w1 + w2);
-                     done = false;
-                     break;
-                 }
-             }
-             
-         }
-         logger.debug(" RED: " + gs);
-         for (DefaultWeightedEdge edge : gs.edgeSet()) {
-         logger.debug(edge + " " + gs.getEdgeWeight(edge));
-         }
-         FloydWarshallShortestPaths<Vertex, DefaultWeightedEdge> paths = new FloydWarshallShortestPaths<>(gs);
-    }
 
     static public class Result {
 
-        Result(Matrix aM, UndirectedGraph<Vertex, DefaultEdge> aG, GraphPath<Vertex, DefaultEdge> aP) {
+        Result(Matrix aM, UndirectedGraph<IntVertex, DefaultEdge> aG, GraphPath<IntVertex, DefaultEdge> aP) {
             m = aM;
             graph = aG;
             path = aP;
         }
 
         Matrix m;
-        UndirectedGraph<Vertex, DefaultEdge> graph;
-        GraphPath<Vertex, DefaultEdge> path;
+        UndirectedGraph<IntVertex, DefaultEdge> graph;
+        GraphPath<IntVertex, DefaultEdge> path;
     }
 
     private Result runLongestShortestPaths(Matrix aImageMatrix) {
-        List<List<Integer>> conn = findAllElementsOfObject(aImageMatrix, true);
-        UndirectedGraph<Vertex, DefaultEdge> g = new SimpleGraph<>(DefaultEdge.class);
+        UndirectedGraph<IntVertex, DefaultEdge> g = GraphUtils.matrixToGraph(aImageMatrix, true);
+        UndirectedGraph<IntVertex, DefaultEdge> gMst = GraphUtils.minimumSpanningTree(g);
 
-        for (int i = 0; i < conn.size(); ++i) {
-            int idx = conn.get(i).get(0);
-            Vertex v = new Vertex(idx);
-            g.addVertex(v);
+        GraphPath<IntVertex, DefaultEdge> path = findPathBoosted(gMst);
 
-            for (int z = 1; z < conn.get(i).size(); z++) {
-                int idxz = conn.get(i).get(z);
-                Vertex vz = new Vertex(idxz);
-                g.addVertex(vz);
-                g.addEdge(v, vz);
-            }
-        }
-
-         KruskalMinimumSpanningTree<Vertex, DefaultEdge> mst = new KruskalMinimumSpanningTree<>(g);
-         UndirectedGraph<Vertex, DefaultEdge> gMst = new SimpleGraph<>(DefaultEdge.class);
-         for (Vertex v : g.vertexSet()) gMst.addVertex(v);
-         for (DefaultEdge e : mst.getEdgeSet()) gMst.addEdge(g.getEdgeSource(e), g.getEdgeTarget(e));
-
-         
-//         graphShow(g.vertexSet(), w, h, "G");
-//         graphShow(gMst.vertexSet(), w, h, "GMST");
-         
-//        UndirectedGraph<Vertex, DefaultEdge> 
-        gMst = g;
-
-        GraphPath<Vertex, DefaultEdge> path = findPathBoosted(gMst);
-//        GraphPath<Vertex, DefaultEdge> path = findPath(gMst);
-        
         Matrix result = aImageMatrix.copy().zeros();
         if (path == null) return new Result(result, null, null);
         for (DefaultEdge e : path.getEdgeList()) {
-            result.set(gMst.getEdgeSource(e).x, 255);
-            result.set(gMst.getEdgeTarget(e).x, 255);
+            result.set(gMst.getEdgeSource(e).getLabel(), 255);
+            result.set(gMst.getEdgeTarget(e).getLabel(), 255);
         }
 
         return new Result(result, gMst, path);
     }
-    
-    private void graphShow(Set<Vertex> aVertex, int w, int h, String aTitle) {
+
+    private void graphShow(Set<IntVertex> aVertex, int w, int h, String aTitle) {
         
         Matrix result = new Matrix(h, w);
-        for (Vertex v : aVertex) {
-            result.set(v.x, 255);
+        for (IntVertex v : aVertex) {
+            result.set(v.getLabel(), 255);
         }
         
         matrixToImage(result, aTitle);
     }
     
-    private GraphPath<Vertex, DefaultEdge> findPath(UndirectedGraph<Vertex, DefaultEdge> gMst) {
-        FloydWarshallShortestPaths<Vertex, DefaultEdge> paths = new FloydWarshallShortestPaths<>(gMst);
-        GraphPath<Vertex, DefaultEdge> path = findLongestPath(gMst.vertexSet().iterator().next(), paths);
+    private GraphPath<IntVertex, DefaultEdge> findPath(UndirectedGraph<IntVertex, DefaultEdge> gMst) {
+        FloydWarshallShortestPaths<IntVertex, DefaultEdge> paths = new FloydWarshallShortestPaths<>(gMst);
+        GraphPath<IntVertex, DefaultEdge> path = findLongestPath(gMst.vertexSet().iterator().next(), paths);
         
         return path;
     }
     
-    private GraphPath<Vertex, DefaultEdge> findPathBoosted(UndirectedGraph<Vertex, DefaultEdge> gMst) {
-        WeightedGraph<Vertex, DefaultWeightedEdge> gs = new SimpleWeightedGraph<>(DefaultWeightedEdge.class);
-        for (Vertex v : gMst.vertexSet()) gs.addVertex(v);
-        for (DefaultEdge e : gMst.edgeSet()) {
-            DefaultWeightedEdge edge = gs.addEdge(gMst.getEdgeSource(e), gMst.getEdgeTarget(e));
-            gs.setEdgeWeight(edge, 1);
-        }
-        boolean done = false;
-        while (!done) {
-            done = true;
-            for (Vertex v  : gs.vertexSet()) {
-                Set<DefaultWeightedEdge> edges = gs.edgesOf(v);
-                if (edges.size() == 2) {
-                    DefaultWeightedEdge[] e = edges.toArray(new DefaultWeightedEdge[0]);
-                    double w1 = gs.getEdgeWeight(e[0]);
-                    double w2 = gs.getEdgeWeight(e[1]);
-                    Vertex es1 = gs.getEdgeSource(e[0]);
-                    Vertex et1 = gs.getEdgeTarget(e[0]);
-                    Vertex es2 = gs.getEdgeSource(e[1]);
-                    Vertex et2 = gs.getEdgeTarget(e[1]);
-                    Vertex ns = (es1.equals(v)) ? et1 : es1;
-                    Vertex nt = (es2.equals(v)) ? et2 : es2;
-                    if (gs.getEdge(ns, nt) != null) continue;
-                    gs.removeVertex(v);
-                    DefaultWeightedEdge newEdge = gs.addEdge(ns, nt);
-                    gs.setEdgeWeight(newEdge, w1 + w2);
-                    done = false;
-                    break;
-                }
-            }
-        }
-        
-//        graphShow(gs.vertexSet(), w, h, "GS");
+    private GraphPath<IntVertex, DefaultEdge> findPathBoosted(UndirectedGraph<IntVertex, DefaultEdge> gMst) {
+        WeightedGraph<IntVertex, DefaultWeightedEdge> gs = GraphUtils.simplifySimipleUndirectedGraph(gMst);
         
         for (DefaultWeightedEdge edge : gs.edgeSet()) {
             logger.debug(edge + " " + gs.getEdgeWeight(edge));
         }
         
-        FloydWarshallShortestPaths<Vertex, DefaultWeightedEdge> paths = new FloydWarshallShortestPaths<>(gs);
-        GraphPath<Vertex, DefaultWeightedEdge> path = findLongestPath(gs.vertexSet().iterator().next(), paths);
+        FloydWarshallShortestPaths<IntVertex, DefaultWeightedEdge> paths = new FloydWarshallShortestPaths<>(gs);
+        GraphPath<IntVertex, DefaultWeightedEdge> path = findLongestPath(gs.vertexSet().iterator().next(), paths);
         if (path == null) return null;
-        DijkstraShortestPath<Vertex, DefaultEdge> dijkstraShortestPath = new DijkstraShortestPath<>(gMst, path.getStartVertex(), path.getEndVertex());
+        DijkstraShortestPath<IntVertex, DefaultEdge> dijkstraShortestPath = new DijkstraShortestPath<>(gMst, path.getStartVertex(), path.getEndVertex());
         
         return dijkstraShortestPath.getPath();
     }
@@ -277,109 +151,6 @@ public class newFilamentsTest extends CommonBase {
         return sum;
     }
 
-    static class Vertex {
-
-        Vertex(int num) {
-            x = num;
-        }
-
-        int x;
-
-        @Override
-        public int hashCode() {
-            final int prime = 31;
-            int result = 1;
-            result = prime * result + x;
-            return result;
-        }
-
-        @Override
-        public boolean equals(Object obj) {
-            if (this == obj) return true;
-            if (obj == null) return false;
-            if (getClass() != obj.getClass()) return false;
-            Vertex other = (Vertex) obj;
-            if (x != other.x) return false;
-            return true;
-        }
-
-        @Override
-        public String toString() {
-            return "{" + x + "}";
-        }
-    }
-
-    private static List<List<Integer>> findAllElementsOfObject(Matrix aMat, boolean aIs8connected) {
-        aMat = Matlab.logical(aMat, 0); // make 0s and 1s
-        // List of found elements belonging to one componnent
-        final List<List<Integer>> elements = new ArrayList<List<Integer>>();
-
-        final double[][] aM = aMat.getArrayXY();
-        final int aWidth = aM.length;
-        final int aHeight = aM[0].length;
-
-        int aStartXpoint = -1;
-        int aStartYpoint = -1;
-        // Go through whole array
-        for (int x = 0; x < aWidth; ++x) {
-            for (int y = 0; y < aHeight; ++y) {
-                if (aM[x][y] != 0) {
-                    aStartXpoint = x;
-                    aStartYpoint = y;
-                }
-            }
-        }
-        if (aStartXpoint < 0) return elements;
-
-        int aLabel = 1234;
-
-        // List of elements to be visited
-        final List<Integer> q = new ArrayList<Integer>();
-
-        // Initialize list with entry point
-        q.add(aStartXpoint * aHeight + aStartYpoint);
-
-        // Iterate until all elements of component are visited
-        while (!q.isEmpty()) {
-            // Get first element on the list and remove it
-            final int id = q.remove(0);
-            final int x = id / aHeight;
-            final int y = id % aHeight;
-
-            // Mark pixel and add it to element's container
-            aM[x][y] = aLabel;
-            List<Integer> ll = new ArrayList<Integer>();
-            ll.add(id);
-            elements.add(ll);
-
-            // Check all neighbours of currently processed pixel
-            // (do some additional logic to skip point itself and to handle 4/8
-            // base connectivity)
-            for (int dx = -1; dx <= 1; dx++) {
-                for (int dy = -1; dy <= 1; dy++) {
-                    if (dx != 0 || dy != 0) {
-                        final int indX = x + dx;
-                        final int indY = y + dy;
-                        if (indX >= 0 && indX < aWidth && indY >= 0 && indY < aHeight) {
-                            if (aIs8connected || (dy * dx == 0)) {
-                                if (aM[indX][indY] == 1) {
-                                    final int idx = indX * aHeight + indY;
-                                    ll.add(idx);
-                                    if (!q.contains(idx)) {
-                                        // If element was not visited yet put it
-                                        // on list
-                                        q.add(idx);
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-
-        return elements;
-    }
     int w;
     int h;
 
@@ -402,8 +173,8 @@ public class newFilamentsTest extends CommonBase {
         input = "/Users/gonciarz/test/single.tif";
         input = "/Users/gonciarz/test/Crop_12-12.tif";
         input = "/Users/gonciarz/test/many.tif";
-        input = "/Users/gonciarz/test/longlong.tif";
         input = "/Users/gonciarz/test/sample.jpg";
+        input = "/Users/gonciarz/test/longlong.tif";
         boolean toBeSegmented = true;
 
         // Load input
@@ -451,7 +222,7 @@ public class newFilamentsTest extends CommonBase {
             mm[i] = result.m;
             logger.info("Splines...");
             // GENERATE SPLINES
-            GraphPath<Vertex, DefaultEdge> path = result.path;
+            GraphPath<IntVertex, DefaultEdge> path = result.path;
             if (path == null) continue;
             int len = path.getEdgeList().size();
             logger.debug("LENGHT: " + len);
@@ -459,15 +230,15 @@ public class newFilamentsTest extends CommonBase {
             double step = (double) len / (max);
             if (step < 1) step = 1;
             List<Integer> pts = new ArrayList<Integer>();
-            pts.add(path.getStartVertex().x);
+            pts.add(path.getStartVertex().getLabel());
             List<DefaultEdge> edgeList = path.getEdgeList();
             logger.debug("STEP: " + step);
             for (double currentIdx = 0; currentIdx < edgeList.size(); currentIdx += step) {
                 if (currentIdx == 0) continue;
-                pts.add(result.graph.getEdgeTarget(edgeList.get((int) currentIdx)).x);
+                pts.add(result.graph.getEdgeTarget(edgeList.get((int) currentIdx)).getLabel());
                 
             }
-            pts.add(path.getEndVertex().x);
+            pts.add(path.getEndVertex().getLabel());
             logger.debug("NUMOF: " + pts.size());
             final List<Double> xv = new ArrayList<Double>();
             final List<Double> yv = new ArrayList<Double>();
